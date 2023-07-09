5a4b08119347373ff35a0590df20271d8f6b5bc6
lenainweb <lenainweb@gmail.com>
2023-07-09 16:32:28 +0200

    add a common interface that we will fill with functionality and improve

---
+++ set_password.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+
+def check_user_exists(username: str) -> bool:
+    """Check if the user exists in the system."""
+    pass
+
+
+def check_user_has_password(username: str) -> bool:
+    """Checking if the user has a password."""
+    pass
+
+
+def input_password() -> str:
+    """
+    We ask the user to enter a password and check it for compliance
+    with the requirements or generate a password automatically.
+    """
+
+
+def validate_password(password: str) -> bool:
+    """Checking the password for compliance."""
+    pass
+
+
+def generate_password() -> str:
+    """Password generation."""
+    pass
+
+
+def set_password(username: str, password: str) -> None:
+    """Setting a user password."""
+    pass
+
+
+def main() -> None:
+    """
+    General logic of the program. We ask for a username,
+    make sure the user exists. Request a password,
+    set a password for a user.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
